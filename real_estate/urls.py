4b867c20dc12fd25fc922bff3062118da725a6cd
Md Azharul Islam Bhuiyan <mdazharulislambhuiyaniiuc@gmail.com>
2024-03-21 03:51:47 +0600

    Add Profile model and signals

--- real_estate/urls.py
+++ real_estate/urls.py
@@ -8,3 +8,7 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+admin.site.site_header = "Real Estate Admin"
+admin.site.site_title = "Real Estate Admin Portal"
+admin.site.index_title = "Welcome to the Real Estate Portal"
